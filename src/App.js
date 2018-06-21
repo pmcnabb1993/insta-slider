703218a42e782f828139a0471c6dfd7d1ff0a9b9
Preston McNabb <prestonmcnabb22@gmail.com>
2018-06-21 16:58:45 -0500

    added superagent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import request from 'superagent';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    }
+  }
+
+  componentWillMount() {
+    this.fetchPhotos();
+  }
+
+  fetchPhotos() {
+    request
+      .get('https://api.instagram.com/v1/users/self/media/recent/?access_token=ACCESS-TOKEN')
+      .then((res) => {
+        this.setState({
+          photos: res.body.data
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +32,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div>
+          {console.log(this.state.photos)}
+        </div>
       </div>
     );
   }
