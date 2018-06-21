2afbe4e323ff2b6bb1f6a3912cacfef9a057527c
Preston McNabb <prestonmcnabb22@gmail.com>
2018-06-21 18:45:11 -0500

    added instagram api and arrow functions

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 import request from 'superagent';
 import logo from './logo.svg';
 import './App.css';
+import 'font-awesome/css/font-awesome.css';
+
+require('dotenv').config()
+
+const BackArrow = (props) => (
+  <div onClick={props.previousImage} style={{fontSize: '2em', marginRight: '12px'}}>
+    <i className="fa fa-angle-left fa-2x" aria-hidden="true"></i>
+  </div>
+)
+
+const NextArrow = (props) => (
+  <div onClick={props.nextImage} style={{fontSize: '2em', marginLeft: '12px'}}>
+    <i className="fa fa-angle-right fa-2x" aria-hidden="true"></i>
+  </div>
+)
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photos: []
+      photos: [],
+      slideCount: 0
     }
+    this.nextImage = this.nextImage.bind(this);
+    this.previousImage = this.previousImage.bind(this);
   }
 
   componentWillMount() {
     this.fetchPhotos();
   }
 
+
   fetchPhotos() {
     request
-      .get('https://api.instagram.com/v1/users/self/media/recent/?access_token=ACCESS-TOKEN')
+      .get('https://api.instagram.com/v1/users/self/media/recent/?access_token=13513630.1677ed0.6a4397a93e4e493994d966a7fa09aeae')
       .then((res) => {
         this.setState({
           photos: res.body.data
@@ -25,30 +44,43 @@ class App extends Component {
       })
   }
 
+  nextImage() {
+    this.setState({ slideCount: this.state.slideCount + 1 })
+    
+  }
+
+  previousImage() {
+    this.setState({ slideCount: this.state.slideCount - 1 })
+    
+  }
+
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to InstaSlider</h1>
         </header>
-        <div>
-          {console.log(this.state.photos)}
-        </div>
-        <div> 
-          {this.state.photos.map((photo,key) => {
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center',marginTop: '30px'}}>
+          {this.state.slideCount !== 0 ? <BackArrow previousImage={this.previousImage}/> : ''}
+          {this.state.photos.map((photo, key) => {
+            if (this.state.photos.indexOf(photo) === this.state.slideCount) {
             return (
-              <div key={photo.id}>
-                <img src={photo.images.standard_resolution.url} alt={photo.caption}/>
-                  <div style={{width:'600px', margin: '24px auto', fontStyle: 'italic'}}>
-                      {photo.caption !== null ? photo.caption.text : ''}
-                  </div>  
-              </div>
-            )
-          })}
-        </div>
-      </div>
+              <div key={photo.id} style={{margin: '0 auto'}}>
+              <img src={photo.images.standard_resolution.url} alt=''/>
+              <div style={{width:'600px', margin: '24px auto', fontStyle: 'italic'}}>
+               {photo.caption !== null ? photo.caption.text : ''}
+             </div>
+           </div>
+         )
+       }
+     return ''
+   })}
+   {this.state.slideCount !== (this.state.photos.length - 1) ? <NextArrow nextImage={this.nextImage}/> : ''} </div>
+    </div>
     );
+
   }
 }
 
